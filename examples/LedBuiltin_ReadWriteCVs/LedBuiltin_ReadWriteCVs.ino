aa2d5e64e449bfbccc7f4eadb9837be75d46f60d
Jindra fucik <fulda@seznam.cz>
2025-09-07 20:48:56 +0200

    Update LedBuiltin_ReadWriteCVs.ino added EEPROM
    added EEPROM support
    translated to English like language

--- examples/LedBuiltin_ReadWriteCVs/LedBuiltin_ReadWriteCVs.ino
+++ examples/LedBuiltin_ReadWriteCVs/LedBuiltin_ReadWriteCVs.ino
@@ -1,43 +1,49 @@
 /*
-*	Questo esempio permette di vedere la corretta decodifica dell'interfaccia SUSI:
-*   -   Accendendo il led incorporato nella scheda quando la Funzione 0 e' attiva.
-*   -   Permette la lettura/scrittura delle CVs salvate nella EEPROM del Microcontrollore
+*	This example shows the correct decoding of the SUSI interface:
+*   -   By turning on the LED built into the board when Function 0 is active.
+*   -   Allows reading/writing of CVs saved in the Microcontroller's EEPROM
 */
 
-#include <stdint.h>     // Libreria per i tipi "uintX_t"
-#include <SUSI2.h>     // Includo la libreria per la gestione della SUSI
-//#include <EEPROM.h>     // Libreria per la gestione della EEPROM interna
-// TBD
+//#include <stdint.h>     // Library with types "uintX_t" (automatically included by Arduino)
+#include <SUSI2.h>        // Include the library for SUSI management
+#include <EEPROM.h>       // Library for managing the internal EEPROM - Note, CH32V003 have no eeprom, emulated version must be used
+                          // Note: The CH32 EEPROM library uses the user storage area, allowing 26 bytes of emulated EEPROM on the CH32V003.
+                          //       but SUSI can call up to 127 addresses + indexed area. Handle with care!
 
-#define LED_BUILTIN PD6
+#define LED_BUILTIN PD6   // CH32003 nano boards can differ. My one have LED on PD6
 
-SUSI2 SUSI(2, 3);      // (CLK pin, DATA pin) il pin di Clock DEVE ESSERE di tipo interrupt, il pin Data puo' essere in pin qualsiasi: compresi gli analogici
+//SUSI2 SUSI(2, 3);      // (CLK pin, DATA pin) this function is for compatiility only
+SUSI2 SUSI;            // new version does not needed pin definition, as it use hardware receiver on PC5 and PC6 pins.
 
-void notifySusiFunc(SUSI_FN_GROUP SUSI_FuncGrp, uint8_t SUSI_FuncState) {                                           // Funzione CallBack che viene invocata quando e' decodificato un comando per le Funzioni
-    switch (SUSI_FuncGrp) {                                                                                         // Scelgo a quale gruppo funzioni appartiene il comando
-        case SUSI_FN_0_4: {                                                                                         // Funzioni da 0 a 4
-            ((SUSI_FuncState & SUSI_FN_BIT_00) ? digitalWrite(LED_BUILTIN, HIGH) : digitalWrite(LED_BUILTIN, LOW)); // se la Funzione 0 e' attiva accendo il LED_BUILTIN
+
+void notifySusiFunc(SUSI_FN_GROUP SUSI_FuncGrp, uint8_t SUSI_FuncState) {                                           // CallBack function that is invoked when a command for Functions is decoded
+    switch (SUSI_FuncGrp) {                                                                                         // Choose which function group the command belongs to
+        case SUSI_FN_0_4: {                                                                                         // Functions 0 to 4
+            ((SUSI_FuncState & SUSI_FN_BIT_00) ? digitalWrite(LED_BUILTIN, HIGH) : digitalWrite(LED_BUILTIN, LOW)); // if Function 0 is active I turn on the LED_BUILTIN
             break;
         }
         default: {}
     }
 }
 
-//uint8_t notifySusiCVRead(uint16_t CV) {                                                                             // Funzione CallBack per leggere il valore di una CV memorizzata
-//    return EEPROM.read(CV);                                                                                         // Restituisce il valore memorizzato nella EEPROM
-//}
+uint8_t notifySusiCVRead(uint16_t CV) {                                                                             // CallBack function to read the value of a stored CV
+    if (CV < EEPROM.length()) {return EEPROM.read(CV);} else {return 255;}                                          // Returns the value stored in EEPROM
 
-//uint8_t notifySusiCVWrite(uint16_t CV, uint8_t Value) {                                                             // Funzione CallBack per scrivere il valore di una CV memorizzata
-//    EEPROM.update(CV, Value);                                                                                       // Se il valore richiesto e' diverso aggiorno la EEPROM, in caso contrario non modifico per non rovinarla
-//
-//    return EEPROM.read(CV);                                                                                         // Restituisco il nuovo valore della EEPROM
-//}
-
-void setup() {                                                                                                      // Setup del Codice
-    pinMode(LED_BUILTIN, OUTPUT);                                                                                   // Imposto il pin a cui e' connesso il LED_BUILTIN come output
-    SUSI.init();                                                                                                    // Avvio la libreria
 }
 
-void loop() {                                                                                                       // Loop del codice
-    SUSI.process();                                                                                                 // Elaboro piu' volte possibile i dati acquisiti dalla libreria
+uint8_t notifySusiCVWrite(uint16_t CV, uint8_t Value) {                                                             // CallBack function to write the value of a stored CV
+                                                                                                                    // If the required value is different I update the EEPROM, otherwise I do not modify it so as not to damage it.
+    if (CV < EEPROM.length()) {EEPROM.write(CV, Value); EEPROM.commit();}       // commit every write is not effective!
+
+    if (CV < EEPROM.length()) {return EEPROM.read(CV);} else {return 255;}                                          // Return the new value of the EEPROM
+}
+
+void setup() {                                                                                                      // Setup Code
+    pinMode(LED_BUILTIN, OUTPUT);                                                                                   // Set the pin to which the LED_BUILTIN is connected as output
+    EEPROM.begin();                                                                                                 // init emulated eeprom
+    SUSI.init();                                                                                                    // Start the library
+}
+
+void loop() {                                                                                                       // Code loop
+    SUSI.process();                                                                                                 // Process the data acquired from the library as many times as possible
 }
