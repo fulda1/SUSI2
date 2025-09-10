23ddd29fb85145d112f3e646537c688db1127a79
Jindra fucik <fulda@seznam.cz>
2025-09-10 17:58:34 +0200

    Update FunctionDecoder.ino

--- examples/FunctionDecoder/FunctionDecoder.ino
+++ examples/FunctionDecoder/FunctionDecoder.ino
@@ -14,6 +14,8 @@
 #define LED_BUILTIN PD6   // CH32003 nano boards can differ. My one have LED on PD6
 #define LED_FA PC0        // LED used as FA output
 #define LED_FB PC1        // LED used as FB output
+#define LED_FCf PC2       // LED used as FC forward output
+#define LED_FCb PC3       // LED used as FC backward output
 
 
 #define HW_VER 10         // Hardware version 1.0
@@ -23,7 +25,9 @@
 //SUSI2 SUSI(2, 3);       // (CLK pin, DATA pin) this function is for compatibility only
 SUSI2 SUSI;               // new version does not needed pin definition, as it use hardware receiver on PC5 and PC6 pins.
 
-uint8_t FAGroup, FABit, FBGroup, FBBit;
+SUSI_FN_GROUP FAGroup, FBGroup, FCGroup;  // Function group for available functions FA, FB, FC
+uint8_t FABit, FBBit, FCBit;     // Function mask for available functions FA, FB, FC
+SUSI_DIRECTION RealDirection;    // real direction store (note, requested and real can differ as per acceleration/deceleration curve)
 
 void notifySusiFunc(SUSI_FN_GROUP SUSI_FuncGrp, uint8_t SUSI_FuncState) {                                           // CallBack function that is invoked when a command for Functions is decoded
     switch (SUSI_FuncGrp) {                                                                                         // Choose which function group the command belongs to
@@ -34,18 +38,31 @@ void notifySusiFunc(SUSI_FN_GROUP SUSI_FuncGrp, uint8_t SUSI_FuncState) {       
         default: {}
     }
 
+      // simple function mapping to output
     if (SUSI_FuncGrp == FAGroup) {((SUSI_FuncState & FABit) ? digitalWrite(LED_FA, HIGH) : digitalWrite(LED_FA, LOW));}
     if (SUSI_FuncGrp == FBGroup) {((SUSI_FuncState & FBBit) ? digitalWrite(LED_FB, HIGH) : digitalWrite(LED_FB, LOW));}
+
+      // Function mapping for direction depend functions
+    if (SUSI_FuncGrp == FCGroup) {
+      if (RealDirection == SUSI_DIR_FWD) { ((SUSI_FuncState & FCBit) ? digitalWrite(LED_FCf, HIGH) : digitalWrite(LED_FCf, LOW)); digitalWrite(LED_FCb, LOW);}
+                                    else { ((SUSI_FuncState & FCBit) ? digitalWrite(LED_FCb, HIGH) : digitalWrite(LED_FCb, LOW)); digitalWrite(LED_FCf, LOW);}
+    }
 }
 
-uint8_t notifySusiCVRead(uint8_t CV, uint8_t CVindex) {                                                     // CallBack function to read the value of a stored CV
+void notifySusiRealSpeed(uint8_t Speed, SUSI_DIRECTION Dir) {                                                       // CallBack function that is invoked when  the Actual Speed ​​and Direction are received
+  RealDirection = Dir;                                                                                              // Save direction information for direction depend functions
+  (void)(Speed);                                                                                                    // Have no usage for parameter "Speed", will mark it as "unused"
+}
+
+uint8_t notifySusiCVRead(uint8_t CV, uint8_t CVindex) {                                                             // CallBack function to read the value of a stored CV
 
     // all CVs are stored as for device #1, once acting as device #2 or #3, change to #1
-    if ((CV>42) && (CV<83)) { return notifySusiCVRead(CV - 40, CVindex); }                                           // CVs for device 2 are same as for device 1
-    if ((CV>82) && (CV<123)) { return notifySusiCVRead(CV - 80, CVindex); }                                          // CVs for device 3 are same as for device 1
+    if ((CV>42) && (CV<83)) { return notifySusiCVRead(CV - 40, CVindex); }                                          // CVs for device 2 are same as for device 1
+    if ((CV>82) && (CV<123)) { return notifySusiCVRead(CV - 80, CVindex); }                                         // CVs for device 3 are same as for device 1
     
-    // List of supported CVs
+    // Mandatory CVs
     if (CV == 0) {return EEPROM.read(0);}                                                                           // 0 = CV #897 module address
+    // List of supported read only (fixed value) CVs
     if ((CV == 3) && (CVindex == 0)) {return MANUFACTER_ID;}                                                        // 3 = CV #900.0 Manufacturer identification / only in Bank 0
     if ((CV == 3) && (CVindex == 0)) {return HW_VER;}                                                               // 3 = CV #900.1 Hardware identification / only in Bank 1
     //if ((CV == 3) && (CVindex == 254)) {return MyAlternativeID;}                                                  // 3 = CV #900.254 Alternative Manufacturer ID / only in Bank 254
@@ -53,20 +70,23 @@ uint8_t notifySusiCVRead(uint8_t CV, uint8_t CVindex) {                         
     if ((CV == 4) && (CVindex == 0)) {return SUBVERSION_NO;}                                                        // 4 = CV #901.1 Subversion number / only in Bank 1
     if ((CV == 4) && (CVindex == 254)) {return SUSI_VER;}                                                           // 4 = CV #901.254 SUSI version / only in Bank 254
 
-    if (CV == 5) {return EEPROM.read(1);}                                                                          // 5 = CV #902 Manufacturer specific / Output #1 function in my case
-    if (CV == 6) {return EEPROM.read(2);}                                                                          // 5 = CV #903 Manufacturer specific / Output #2 function in my case
+    // Manufacturer managed CVs
+    if (CV == 5) {return EEPROM.read(1);}                                                                           // 5 = CV #902 Manufacturer specific / Output #1(A) function in my case
+    if (CV == 6) {return EEPROM.read(2);}                                                                           // 5 = CV #903 Manufacturer specific / Output #2(B) function in my case
+    if (CV == 7) {return EEPROM.read(3);}                                                                           // 6 = CV #904 Manufacturer specific / Output #3(C) function in my case
 
     return 255;                                                                                                     // no other CVs supported
 }
 
-uint8_t notifySusiCVWrite(uint8_t CV, uint8_t CVindex, uint8_t Value) {                                     // CallBack function to write the value of a stored CV
+uint8_t notifySusiCVWrite(uint8_t CV, uint8_t CVindex, uint8_t Value) {                                             // CallBack function to write the value of a stored CV
 
     // all CVs are stored as for device #1, once acting as device #2 or #3, change to #1
-    if ((CV>42) && (CV<83)) { return notifySusiCVWrite(CV - 40, CVindex, Value); }                                   // CVs for device 2 are same as for device 1
-    if ((CV>82) && (CV<123)) { return notifySusiCVWrite(CV - 80, CVindex, Value); }                                  // CVs for device 3 are same as for device 1
+    if ((CV>42) && (CV<83)) { return notifySusiCVWrite(CV - 40, CVindex, Value); }                                  // CVs for device 2 are same as for device 1
+    if ((CV>82) && (CV<123)) { return notifySusiCVWrite(CV - 80, CVindex, Value); }                                 // CVs for device 3 are same as for device 1
     
-    // List of supported CVs READ ONLY
+    // Mandatory CVs
     if (CV == 0) {EEPROM.write(1, Value); EEPROM.commit(); ReadMyCVs(); return EEPROM.read(0);}                     // 0 = CV #897 module address
+    // List of supported CVs READ ONLY -> return value without any action
     if ((CV == 3) && (CVindex == 0)) {return MANUFACTER_ID;}                                                        // 3 = CV #900.0 Manufacturer identification / only in Bank 0
     if ((CV == 3) && (CVindex == 0)) {return HW_VER;}                                                               // 3 = CV #900.1 Hardware identification / only in Bank 1
     //if ((CV == 3) && (CVindex == 254)) {return MyAlternativeID;}                                                  // 3 = CV #900.254 Alternative Manufacturer ID / only in Bank 254
@@ -74,8 +94,10 @@ uint8_t notifySusiCVWrite(uint8_t CV, uint8_t CVindex, uint8_t Value) {         
     if ((CV == 4) && (CVindex == 0)) {return SUBVERSION_NO;}                                                        // 4 = CV #901.1 Subversion number / only in Bank 1
     if ((CV == 4) && (CVindex == 254)) {return SUSI_VER;}                                                           // 4 = CV #901.254 SUSI version / only in Bank 254
 
-    if (CV == 5) {EEPROM.write(1, Value); EEPROM.commit(); ReadMyCVs(); return EEPROM.read(1);}                    // 5 = CV #902 Manufacturer specific / Output #1 function in my case
-    if (CV == 6) {EEPROM.write(2, Value); EEPROM.commit(); ReadMyCVs(); return EEPROM.read(2);}                    // 6 = CV #903 Manufacturer specific / Output #2 function in my case
+    // Manufacturer managed CVs
+    if (CV == 5) {EEPROM.write(1, Value); EEPROM.commit(); ReadMyCVs(); return EEPROM.read(1);}                    // 5 = CV #902 Manufacturer specific / Output #1(A) function in my case
+    if (CV == 6) {EEPROM.write(2, Value); EEPROM.commit(); ReadMyCVs(); return EEPROM.read(2);}                    // 6 = CV #903 Manufacturer specific / Output #2(B) function in my case
+    if (CV == 7) {EEPROM.write(3, Value); EEPROM.commit(); ReadMyCVs(); return EEPROM.read(3);}                    // 7 = CV #904 Manufacturer specific / Output #3(C) function in my case
 	                                                                                        // commit every write is not effective!
 																							
     return 255;                                                                                                     // no other CVs supported
@@ -86,39 +108,50 @@ void ReadMyCVs(void) {                                                          
   MemValue = EEPROM.read(0);                                                                                        // just check, if module number is 1, 2, or 3.
   if ((MemValue < 1) || (MemValue > MAX_ADDRESS_VALUE)) {EEPROM.write(0, DEFAULT_SLAVE_NUMBER); EEPROM.commit(); }  // No, then set default
 
-  MemValue = EEPROM.read(1);                                                                                        // Output #1 function in my case
+  MemValue = EEPROM.read(1);                                                                                        // Output #1(A) function in my case
   if (MemValue > 68) {MemValue=0; EEPROM.write(1, MemValue); EEPROM.commit();}                                      // Check, if output is 0 to 68 - if not, set 0 as default
+  FAGroup = (MemValue + 3) / 8;                                                                                     // this will fill gap of 3 missing functions in group 1
   if (MemValue == 0) {
-    FAGroup = SUSI_FN_0_4;
     FABit = SUSI_FN_BIT_00;
   } else if (MemValue <= 4) {
-    FAGroup = SUSI_FN_0_4;
     FABit = 1 << (MemValue - 1);
   } else {
     MemValue += 3;
-    FAGroup = MemValue / 8;
     FABit = 1 << (MemValue % 8);
   }
 
-  MemValue = EEPROM.read(2);                                                                                        // Output #2 function in my case
+  MemValue = EEPROM.read(2);                                                                                        // Output #2(B) function in my case
   if (MemValue > 68) {MemValue=0; EEPROM.write(2, MemValue); EEPROM.commit();}                                      // Check, if output is 0 to 68 - if not, set 0 as default
+  FBGroup = (MemValue + 3) / 8;                                                                                     // this will fill gap of 3 missing functions in group 1
   if (MemValue == 0) {
-    FBGroup = SUSI_FN_0_4;
     FBBit = SUSI_FN_BIT_00;
   } else if (MemValue <= 4) {
-    FBGroup = SUSI_FN_0_4;
     FBBit = 1 << (MemValue - 1);
   } else {
     MemValue += 3;
-    FBGroup = MemValue / 8;
     FBBit = 1 << (MemValue % 8);
   }
+
+  MemValue = EEPROM.read(3);                                                                                        // Output #3(C) function in my case
+  if (MemValue > 68) {MemValue=0; EEPROM.write(3, MemValue); EEPROM.commit();}                                      // Check, if output is 0 to 68 - if not, set 0 as default
+  FCGroup = (MemValue + 3) / 8;                                                                                     // this will fill gap of 3 missing functions in group 1
+  if (MemValue == 0) {
+    FCBit = SUSI_FN_BIT_00;
+  } else if (MemValue <= 4) {
+    FCBit = 1 << (MemValue - 1);
+  } else {
+    MemValue += 3;
+    FCBit = 1 << (MemValue % 8);
+  }
+
 }
 
 void setup() {                                                                                                      // Setup Code
     pinMode(LED_BUILTIN, OUTPUT);                                                                                   // Set the pin to which the LED_BUILTIN is connected as output
     pinMode(LED_FA, OUTPUT);                                                                                        // Set the pin to which the LED_FA is connected as output
     pinMode(LED_FB, OUTPUT);                                                                                        // Set the pin to which the LED_FB is connected as output
+    pinMode(LED_FCf, OUTPUT);                                                                                        // Set the pin to which the LED_FB is connected as output
+    pinMode(LED_FCb, OUTPUT);                                                                                        // Set the pin to which the LED_FB is connected as output
     EEPROM.begin();                                                                                                 // init emulated eeprom
 	  ReadMyCVs();                                                                                                    // Read local CVs to working variables
     SUSI.init();                                                                                                    // Start the library
